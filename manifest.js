d36fe2b322a63563abf0430a9e1c9f6b8190af22
Molnár Vilmos <66210056+moln4rvilmos@users.noreply.github.com>
2024-11-15 09:13:38 +0100

    manifest.js létrehozása
    A manifest.json fájl olyan metaadatokat tartalmaz, amelyek meghatározzák, hogyan jelenik meg az alkalmazás (ikon, név, színséma stb.), amikor a felhasználó hozzáadja a kezdőképernyőhöz.

---
+++ manifest.js
@@ -0,0 +1,21 @@
+{
+  "name": "Vilix.hu",
+  "short_name": "Vilix",
+  "start_url": "/",
+  "display": "standalone",
+  "background_color": "#d0d6e0",
+  "theme_color": "#0000ff",
+  "description": "Mindennapi tartalom a mai dátum szerint.",
+  "icons": [
+    {
+      "src": "/icons/icon-192x192.png",
+      "sizes": "192x192",
+      "type": "image/png"
+    },
+    {
+      "src": "/icons/icon-512x512.png",
+      "sizes": "512x512",
+      "type": "image/png"
+    }
+  ]
+}
